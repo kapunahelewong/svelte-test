557ce38f523a23191acab4f7bf4f3a88e7f7ea8c
Kapunahele Wong <kapunahelewong@gmail.com>
2023-11-09 11:41:03 -0800

    add docs code and API Key

---
+++ src/routes/*page.server.js
@@ -0,0 +1,26 @@
+// src/routes/+page.server.js
+import { fetchOneEntry } from '@builder.io/sdk-svelte';
+
+export async function load({ page }) {
+  if (!page) {
+    page = []; // Set a default value if page is not provided
+  }
+
+  const urlPath = `/${page.join('/')}`;
+
+  // fetch your Builder content
+  const content = await fetchOneEntry({
+    model: 'page',
+    apiKey: "fe07520489dd4741b861fe3c2e19b071",
+    userAttributes: {
+      urlPath,
+    },
+  });
+
+  // return content from `fetchOneEntry()`
+  return {
+    props: {
+      content,
+    },
+  };
+}
---
+++ src/routes/+page.svelte
@@ -0,0 +1,21 @@
+<!-- src/routes/+page.svelte -->
+<script>
+  import { RenderContent } from "@builder.io/sdk-svelte";
+
+  // Add your Public API Key
+  const YOUR_API_KEY = "fe07520489dd4741b861fe3c2e19b071";
+
+  export let content;
+</script>
+
+<main>
+  <h1>Welcome to your Builder-integrated Svelte app</h1>
+  <div>Below is your Builder Content:</div>
+
+  {#if data.content || isPreviewing()}
+    <!-- Render builder content with all required props -->
+    <RenderContent model="page" {content} apiKey={YOUR_API_KEY} />
+  {:else}
+    <p>Loading...</p>
+  {/if}
+</main>
