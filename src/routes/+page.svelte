cd72b329ac889444c8d68a0911c1c085d1af227d
Kapunahele Wong <kapunahelewong@gmail.com>
2023-11-09 17:29:39 -0800

    integrates but {content} not right. Add troubleshooting code

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,4 @@
+<!-- `content` only returns one page (though there are 3 in that space) but no content is displayed -->
 <!-- src/routes/+page.svelte -->
 <script lang="ts">
   import { Content, fetchEntries, isPreviewing } from "@builder.io/sdk-svelte";
@@ -6,30 +7,25 @@
   const YOUR_API_KEY = "fe07520489dd4741b861fe3c2e19b071";
   let content = undefined;
   let canShowContent = false;
-  const fetch = async () => {
-    // fetch your Builder content
-    content = await fetchEntries({
-      model: "page",
-      apiKey: YOUR_API_KEY,
-      userAttributes: {
-        urlPath: window.location.pathname || "/",
-      },
-    });
-    // handle preview mode
-    canShowContent = content || isPreviewing();
-  };
 
   // Define an async function to fetch and log pages
   const fetchAndLogPages = async () => {
     try {
       // Fetch all pages from Builder
-      const pages = await fetchEntries({
+      content = await fetchEntries({
         model: "page", // Change 'page' to match your Builder model name
         apiKey: YOUR_API_KEY,
+        userAttributes: {
+          urlPath: window.location.pathname || "/",
+        },
       });
 
-      // Log the pages
-      console.log("Pages from Builder:", pages);
+      // Log the entire response
+      console.log("Content from Builder:", content);
+
+      // Set canShowContent based on whether content is defined
+      canShowContent = !!content;
+      console.log("canShowContent:", canShowContent);
     } catch (error) {
       console.error("Error fetching pages from Builder:", error);
     }
@@ -44,8 +40,10 @@
   <div>Below is your Builder Content:</div>
 
   {#if canShowContent}
-    <div>page Title: {content?.data?.title || "Unpublished"}</div>
+    <div>page Title: {content?.results?.[0].data?.title || "Unpublished"}</div>
     <!-- Render builder content with all required props -->
+
+    <!-- This doesn't show anything -->
     <Content model="page" {content} apiKey={YOUR_API_KEY} />
   {:else}
     <p>Loading...</p>
